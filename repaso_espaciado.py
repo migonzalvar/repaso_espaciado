ceef721e921ed702db3b14a3d0a96de56c6f079d
Miguel Gonzalez <migonzalvar@gmail.com>
2022-11-04 13:38:44 +0100

    Cambia mazos a diccionario

--- repaso_espaciado.py
+++ repaso_espaciado.py
@@ -1,15 +1,15 @@
 # repaso_espaciado.py
 
-mazos = ["Capitales", "Marcas de coche", "Ríos"]
-tarjetas_iniciales = [
-    ("España", "Madrid"),
-    ("Portugal", "Lisboa"),
-    ("Francia", "París"),
-]
+mazos = {
+    "Capitales": [("España", "Madrid"), ("Portugal", "Lisboa"), ("Francia", "París")],
+    "Coches": [("Tesla", "Estados Unidos")],
+    "Ríos": [],
+}
 
 while True:
     print("Escoge un mazo:")
-    for n, mazo in enumerate(mazos):
+    nombre_de_mazos = list(mazos.keys())
+    for n, mazo in enumerate(nombre_de_mazos):
         print(n, mazo)
 
     selección = input("? ")
@@ -18,25 +18,21 @@ while True:
     índice = int(selección)
     print("[DEBUG]", f"{índice=}")
 
-    print("Has seleccionado", mazos[índice])
+    nombre_de_mazo = nombre_de_mazos[índice]
 
-    if índice == 0:
-        tarjetas_en_mano = tarjetas_iniciales
-        tarjetas_difíciles = []
-        while len(tarjetas_en_mano) > 0:
-            tarjeta = tarjetas_en_mano.pop(0)
-            pregunta, respuesta = tarjeta
-            input(f"¿Cuál es la capital de {pregunta}? Pulsa ENTER para comprobar la respuesta.")
-            print(f"La respuesta es: {respuesta}.")
-            valoración = input("¿1 Fácil / 2 Difícil / 3 Repetir? ")
-            if valoración == "2":
-                tarjetas_difíciles.append(tarjeta)
-            elif valoración == "3":
-                tarjetas_en_mano.append(tarjeta)
+    print("Has seleccionado", nombre_de_mazo)
 
-        tarjetas_iniciales = tarjetas_difíciles
+    tarjetas_en_mano = mazos[nombre_de_mazo]
+    tarjetas_difíciles = []
+    while len(tarjetas_en_mano) > 0:
+        tarjeta = tarjetas_en_mano.pop(0)
+        pregunta, respuesta = tarjeta
+        input(f"¿Cuál es la capital de {pregunta}? Pulsa ENTER para comprobar la respuesta.")
+        print(f"La respuesta es: {respuesta}.")
+        valoración = input("¿1 Fácil / 2 Difícil / 3 Repetir? ")
+        if valoración == "2":
+            tarjetas_difíciles.append(tarjeta)
+        elif valoración == "3":
+            tarjetas_en_mano.append(tarjeta)
 
-    elif índice == 1:
-        ...
-    else:
-        print("Mazo no disponible")
+    mazos[nombre_de_mazo] = tarjetas_difíciles
