f6aa3c49987e9e63be19bdbe19e392536f427aa4
Miguel Gonzalez <migonzalvar@gmail.com>
2022-11-04 10:41:14 +0100

    Añade preguntas y respuestas

--- repaso_espaciado.py
+++ repaso_espaciado.py
@@ -12,3 +12,17 @@ print("[DEBUG]", f"{selección=}")
 print("[DEBUG]", f"{índice=}")
 
 print("Has seleccionado", mazos[índice])
+
+if índice == 0:
+    preguntas = ["España", "Portugal", "Francia"]
+    respuestas = ["Madrid", "Lisboa", "París"]
+
+    for i in range(len(preguntas)):
+        input(f"¿Cuál es la capital de {preguntas[i]}? Pulsa ENTER para comprobar la respuesta.")
+        print(f"La respuesta es: {respuestas[i]}.")
+        valoración = input("¿Fácil/Difícil/Repetir? ")
+
+elif índice == 1:
+    ...
+else:
+    print("Mazo no disponible")
