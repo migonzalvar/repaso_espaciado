885958ca3d836d4dc8d6f6c7ce1a11320830cc2b
Miguel Gonzalez <migonzalvar@gmail.com>
2022-11-04 12:39:41 +0100

    Implementa tarjeta difícil

--- repaso_espaciado.py
+++ repaso_espaciado.py
@@ -1,35 +1,42 @@
 # repaso_espaciado.py
 
 mazos = ["Capitales", "Marcas de coche", "Ríos"]
-print("Escoge un mazo:")
-for n, mazo in enumerate(mazos):
-    print(n, mazo)
+tarjetas_iniciales = [
+    ("España", "Madrid"),
+    ("Portugal", "Lisboa"),
+    ("Francia", "París"),
+]
 
-selección = input("? ")
-print("[DEBUG]", f"{selección=}")
+while True:
+    print("Escoge un mazo:")
+    for n, mazo in enumerate(mazos):
+        print(n, mazo)
 
-índice = int(selección)
-print("[DEBUG]", f"{índice=}")
+    selección = input("? ")
+    print("[DEBUG]", f"{selección=}")
 
-print("Has seleccionado", mazos[índice])
+    índice = int(selección)
+    print("[DEBUG]", f"{índice=}")
 
-if índice == 0:
-    tarjetas = [
-        ("España", "Madrid"),
-        ("Portugal", "Lisboa"),
-        ("Francia", "París"),
-    ]
+    print("Has seleccionado", mazos[índice])
 
-    while len(tarjetas) > 0:
-        tarjeta = tarjetas.pop(0)
-        pregunta, respuesta = tarjeta
-        input(f"¿Cuál es la capital de {pregunta}? Pulsa ENTER para comprobar la respuesta.")
-        print(f"La respuesta es: {respuesta}.")
-        valoración = input("¿1 Fácil /2 Difícil/3 Repetir? ")
-        if valoración == "3":
-            tarjetas.append(tarjeta)
+    if índice == 0:
+        tarjetas_en_mano = tarjetas_iniciales
+        tarjetas_difíciles = []
+        while len(tarjetas_en_mano) > 0:
+            tarjeta = tarjetas_en_mano.pop(0)
+            pregunta, respuesta = tarjeta
+            input(f"¿Cuál es la capital de {pregunta}? Pulsa ENTER para comprobar la respuesta.")
+            print(f"La respuesta es: {respuesta}.")
+            valoración = input("¿1 Fácil / 2 Difícil / 3 Repetir? ")
+            if valoración == "2":
+                tarjetas_difíciles.append(tarjeta)
+            elif valoración == "3":
+                tarjetas_en_mano.append(tarjeta)
 
-elif índice == 1:
-    ...
-else:
-    print("Mazo no disponible")
+        tarjetas_iniciales = tarjetas_difíciles
+
+    elif índice == 1:
+        ...
+    else:
+        print("Mazo no disponible")
