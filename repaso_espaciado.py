3eb0ce7ef20826af44ab785d00cd18b9d1b40d55
Miguel Gonzalez <migonzalvar@gmail.com>
2022-11-10 10:03:23 +0100

    Mezcla diccionarios en uno

--- repaso_espaciado.py
+++ repaso_espaciado.py
@@ -1,20 +1,25 @@
 # repaso_espaciado.py
 
-mazos = {
-    "Capitales": [("España", "Madrid"), ("Portugal", "Lisboa"), ("Francia", "París")],
-    "Coches": [("Tesla", "Estados Unidos")],
-    "Ríos": [("Miño", "Galicia")],
-}
-
-plantillas = {
-    "Capitales": "¿Cuál es la capital de {pregunta}?",
-    "Coches": "¿De qué país es {pregunta}?",
-    "Ríos": "¿Dónde desemboca el {pregunta}?",
+MAZOS = {
+    "Capitales": {
+        "plantilla": "¿Cuál es la capital de {pregunta}?",
+        "tarjetas": [("España", "Madrid"), ("Portugal", "Lisboa"), ("Francia", "París")],
+    },
+    "Coches": {
+        "plantilla": "¿De qué país es {pregunta}?",
+        "tarjetas": [("Tesla", "Estados Unidos")],
+    },
+    "Ríos": {
+        "plantilla": "¿Dónde desemboca el {pregunta}?",
+        "tarjetas": [("Miño", "Galicia")],
+    },
 }
 
 while True:
+    print("[DEBUG]", f"{MAZOS=}")
+
     print("Escoge un mazo:")
-    nombre_de_mazos = list(mazos.keys())
+    nombre_de_mazos = list(MAZOS.keys())
     for n, mazo in enumerate(nombre_de_mazos):
         print(n, mazo)
 
@@ -28,12 +33,13 @@ while True:
 
     print("Has seleccionado", nombre_de_mazo)
 
-    tarjetas_en_mano = mazos[nombre_de_mazo]
+    tarjetas_en_mano = MAZOS[nombre_de_mazo]["tarjetas"]
     tarjetas_difíciles = []
     while len(tarjetas_en_mano) > 0:
         tarjeta = tarjetas_en_mano.pop(0)
         pregunta, respuesta = tarjeta
-        mensaje = plantillas[nombre_de_mazo].format(pregunta=pregunta)
+        plantilla = MAZOS[nombre_de_mazo]["plantilla"]
+        mensaje = plantilla.format(pregunta=pregunta)
         input(f"{mensaje} Pulsa ENTER para comprobar la respuesta.")
         print(f"La respuesta es: {respuesta}.")
         valoración = input("¿1 Fácil / 2 Difícil / 3 Repetir? ")
@@ -42,4 +48,4 @@ while True:
         elif valoración == "3":
             tarjetas_en_mano.append(tarjeta)
 
-    mazos[nombre_de_mazo] = tarjetas_difíciles
+    MAZOS[nombre_de_mazo]["tarjetas"] = tarjetas_difíciles
