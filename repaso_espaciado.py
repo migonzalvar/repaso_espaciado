83322d39d736a5d5d0d0d27bf57a37b8250512cf
Miguel Gonzalez <migonzalvar@gmail.com>
2022-11-11 10:09:40 +0100

    Extrae funciones

--- repaso_espaciado.py
+++ repaso_espaciado.py
@@ -15,6 +15,22 @@ MAZOS = {
     },
 }
 
+def obtén_valoración():
+    texto = "¿1 Fácil / 2 Difícil / 3 Repetir? "
+    opciones = ["1", "2", "3"]
+    valor = input(texto)
+    while valor not in opciones:
+        print("El valor no es correcto, vuelve a intenterlo.")
+        valor = input(texto)
+    return valor
+
+def imprime_tarjeta(plantilla, tarjeta):
+    "Esta función muestra en pantalla la pregunta y sus posibles respuestas."
+    pregunta, respuesta = tarjeta
+    mensaje = plantilla.format(pregunta=pregunta)
+    input(f"{mensaje} Pulsa ENTER para comprobar la respuesta.")
+    print(f"La respuesta es: {respuesta}.")
+
 def main():
     while True:
         mazos_con_tarjetas = []
@@ -47,23 +63,24 @@ def main():
 
         print("Has seleccionado", mazo_escogido)
 
+        plantilla = MAZOS[mazo_escogido]["plantilla"]
         tarjetas_en_mano = MAZOS[mazo_escogido]["tarjetas"]
         tarjetas_difíciles = []
         while len(tarjetas_en_mano) > 0:
             tarjeta = tarjetas_en_mano.pop(0)
-            pregunta, respuesta = tarjeta
-            plantilla = MAZOS[mazo_escogido]["plantilla"]
-            mensaje = plantilla.format(pregunta=pregunta)
-            input(f"{mensaje} Pulsa ENTER para comprobar la respuesta.")
-            print(f"La respuesta es: {respuesta}.")
-            valoración = input("¿1 Fácil / 2 Difícil / 3 Repetir? ")
-            if valoración == "2":
+            imprime_tarjeta(plantilla, tarjeta)
+            valoración = obtén_valoración()
+            if valoración == "1":
+                # Fácil, ;la descarta
+                pass
+            elif valoración == "2":
                 tarjetas_difíciles.append(tarjeta)
             elif valoración == "3":
                 tarjetas_en_mano.append(tarjeta)
+            else:
+                raise ValueError("Opción incorrecta")
 
         MAZOS[mazo_escogido]["tarjetas"] = tarjetas_difíciles
-
 
 if __name__ == "__main__":
     main()
